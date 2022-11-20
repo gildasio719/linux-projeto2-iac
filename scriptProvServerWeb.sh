1be90a09e3ad8b3c5d9b4dc58cfc7b3731c149b3
gildasio719 <gag041984@gmail.com>
2022-11-19 22:24:30 -0300

    Corrigindo nome

---
+++ scriptProvServerWeb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Autor Gildasio de Araujo
+
+echo ">>> Atualizando o servidor, por favos aguarde...<<<"
+apt-get update 
+apt-get upgrade -y
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo ">>> Fazendo Download da aplicacao, por favos aguarde...<<<"
+
+cd /tmp 
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R /var/www/html/
+ll
+
+
